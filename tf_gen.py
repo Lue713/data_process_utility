ae92b0626ae3f03df3955c64d153a075e751a3e4
Yuyue Lv <yl3968@columbia.edu>
2021-08-25 22:13:11 +0800

    add basic funciton defination of TFGen

--- tf_gen.py
+++ tf_gen.py
@@ -8,11 +8,17 @@ class TFGen(object):
 
     def load_data(self, file_path):
         raw_data = pd.read_table(file_path, sep=',')
-        print(raw_data)
+        print(raw_data['test1'])
 
     def my_FFT(self):
         pass
 
     def draw_bode_plot(self):
+        pass
+
+    def diff_forward(self, colum_name, order = 1):
+        pass
+
+    def diff_backward(self,column_name, order = 1):
         pass
 
