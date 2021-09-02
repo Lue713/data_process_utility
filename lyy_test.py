af2f9d3b9c06e9fa64353e0f65d74dbbf5baf384
Yuyue Lv <yl3968@columbia.edu>
2021-09-02 13:09:07 +0800

    add cursor

--- lyy_test.py
+++ lyy_test.py
@@ -4,3 +4,83 @@ tfGen = TFGen()
 tfGen.load_data('./data/test.csv')
 tfGen.diff_forward('test1', 'test1_vel')
 tfGen.show_current_data()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# class Cursor(object):
+#     def __init__(self, ax):
+#         self.ax = ax
+#         self.lx = ax.axhline(color='k')  # the horiz line
+#         self.ly = ax.axvline(color='k')  # the vert line
+#
+#         # text location in axes coords
+#         self.txt = ax.text(0.7, 0.9, '', transform=ax.transAxes)
+#
+#     def mouse_move(self, event):
+#         if not event.inaxes:
+#             return
+#
+#         x, y = event.xdata, event.ydata
+#         # update the line positions
+#         self.lx.set_ydata(y)
+#         self.ly.set_xdata(x)
+#
+#         self.txt.set_text('x=%1.11f\ny=%1.11f' % (x, y))
+#         plt.draw()
+#
+# class SnaptoCursor(object):
+#     """
+#     Like Cursor but the crosshair snaps to the nearest x,y point
+#     For simplicity, I'm assuming x is sorted
+#     """
+#
+#     def __init__(self, ax, x, y):
+#         self.ax = ax
+#         self.lx = ax.axhline(color='k')  # the horiz line
+#         self.ly = ax.axvline(color='k')  # the vert line
+#         self.x = x
+#         self.y = y
+#         # text location in axes coords
+#         self.txt = ax.text(0.7, 0.9, '', transform=ax.transAxes)
+#
+#     def mouse_move(self, event):
+#
+#         if not event.inaxes:
+#             return
+#
+#         x, y = event.xdata, event.ydata
+#
+#         indx = min(np.searchsorted(self.x, [x])[0], len(self.x) - 1)
+#         x = self.x[indx]
+#         y = self.y[indx]
+#         # update the line positions
+#         self.lx.set_ydata(y)
+#         self.ly.set_xdata(x)
+#
+#         self.txt.set_text('x=%1.11f\ny=%1.11f' % (x, y))
+#         print('x=%1.11f\ny=%1.11f' % (x, y))
+#         plt.draw()
+#
+# t = np.arange(0.0,1.0,0.01)
+# s = np.sin(2*np.pi*t)
+# #  make line red
+# fig, ax = plt.subplots()
+# plt.rcParams['lines.color'] = 'r'
+# ax.plot(t,s)
+# # plt.show()
+# #
+# c = np.cos(2*np.pi*t)
+# #
+# # #make line thick
+# plt.rcParams['lines.linewidth'] = '1'
+# plt.plot(t,c)
+# plt.xlabel('t')
+# plt.ylabel('cos(2*pi*t)')
+# plt.title('Plot test')
+#
+# cursor = Cursor(ax)
+# # cursor = SnaptoCursor(ax, t, s)
+# plt.connect('motion_notify_event', cursor.mouse_move)
+# plt.show()
+
--- tf_gen.py
+++ tf_gen.py
@@ -4,19 +4,13 @@ import pandas as pd
 
 class TFGen(object):
     def __init__(self):
-        self.raw_data_column_num = 0
-        self.raw_data_row_num = 0
-        self.raw_data = None
-        self.fine_data = None
+        self.all_data = {}
 
     def load_data(self, file_path):
-        self.raw_data = pd.read_table(file_path, sep=',')
-        self.raw_data_column_num = self.raw_data.shape[1]
-        self.raw_data_row_num = self.raw_data.shape[0]
-        index_column =[i for i in range(self.raw_data_row_num)]
-        self.fine_data = pd.DataFrame(index_column, columns=['index'])
-        # print(self.raw_data)
-        # print(self.fine_data)
+        self.all_data['raw_data'] = pd.read_table(file_path, sep=',')
+        index_column =[i for i in range(self.all_data['raw_data'].shape[0])]
+        self.all_data['processed_data'] = pd.DataFrame(index_column, columns=['index'])
+        # print(self.all_data['processed_data'])
 
     def my_FFT(self):
         pass
@@ -25,26 +19,86 @@ class TFGen(object):
         pass
 
     def diff_forward(self, input_column_name, output_column_name, order = 1):
+        raw_data = self.all_data['raw_data']
+        processed_data = self.all_data['processed_data']
+
         if order is 1:
-            # raw_column_data = np.array(self.raw_data[input_column_name])
-            raw_column_data = self.raw_data[input_column_name].tolist()
+            raw_column_data = raw_data[input_column_name].tolist()
             diffed_data = np.diff(raw_column_data).tolist()
             diffed_data = [diffed_data[0]] + diffed_data
             # self.fine_data.insert(self.fine_data.shape[1], output_column_name, diffed_data)
-            self.fine_data[output_column_name] = diffed_data
+            processed_data[output_column_name] = diffed_data
             # print(self.fine_data)
         elif order is 2:
             print('order not support yet')
         else:
             print('order not support yet')
 
-    def diff_backward(self,column_name, order = 1):
-        pass
+        self.all_data['processed_data'] = processed_data
 
-    def show_current_data(self):
-        print('raw_data:', self.raw_data.columns.values.tolist())
-        print('fine_data:', self.fine_data.columns.values.tolist())
+    def show_current_data(self, show_data = False):
+        data_key_list = list(self.all_data)
+        for name in data_key_list:
+            print(name,':')
+            if show_data:
+                print(self.all_data[name],'\n')
+            else:
+                print(self.all_data[name].columns.values.tolist())
 
     def plot_with_cursor(self):
         pass
 
+
+class Cursor(object):
+    def __init__(self, ax):
+        self.ax = ax
+        self.lx = ax.axhline(color='k')  # the horiz line
+        self.ly = ax.axvline(color='k')  # the vert line
+
+        # text location in axes coords
+        self.txt = ax.text(0.7, 0.9, '', transform=ax.transAxes)
+
+    def mouse_move(self, event):
+        if not event.inaxes:
+            return
+
+        x, y = event.xdata, event.ydata
+        # update the line positions
+        self.lx.set_ydata(y)
+        self.ly.set_xdata(x)
+
+        self.txt.set_text('x=%1.11f\ny=%1.11f' % (x, y))
+        plt.draw()
+
+class SnaptoCursor(object):
+    """
+    Like Cursor but the crosshair snaps to the nearest x,y point
+    For simplicity, I'm assuming x is sorted
+    """
+
+    def __init__(self, ax, x, y):
+        self.ax = ax
+        self.lx = ax.axhline(color='k')  # the horiz line
+        self.ly = ax.axvline(color='k')  # the vert line
+        self.x = x
+        self.y = y
+        # text location in axes coords
+        self.txt = ax.text(0.7, 0.9, '', transform=ax.transAxes)
+
+    def mouse_move(self, event):
+
+        if not event.inaxes:
+            return
+
+        x, y = event.xdata, event.ydata
+
+        indx = min(np.searchsorted(self.x, [x])[0], len(self.x) - 1)
+        x = self.x[indx]
+        y = self.y[indx]
+        # update the line positions
+        self.lx.set_ydata(y)
+        self.ly.set_xdata(x)
+
+        self.txt.set_text('x=%1.11f\ny=%1.11f' % (x, y))
+        print('x=%1.11f\ny=%1.11f' % (x, y))
+        plt.draw()
