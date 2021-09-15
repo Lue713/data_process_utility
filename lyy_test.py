e50092cb6531b628bb058856673df835ccab1ff8
Yuyue Lv <yl3968@columbia.edu>
2021-09-15 22:13:14 +0800

    add fft function

--- lyy_test.py
+++ lyy_test.py
@@ -3,19 +3,34 @@ import numpy as np
 import pandas as pd
 
 from tf_gen import TFGen
+import scipy as sp
+from matplotlib import pyplot as plt
 
-time = np.arange(0.0,1.0,0.01)
+time = np.arange(0.0,2.0,0.01)
 sin = np.sin(2*np.pi*time)
 cos = np.cos(2*np.pi*time)
 #
-# test_data = pd.DataFrame(time, columns = ['time'])
-# test_data['sin'] = sin
-# test_data['cos'] = cos
-# test_data.to_csv('data/test.csv', index = False)
+test_data = pd.DataFrame(time, columns = ['time'])
+test_data['sin'] = sin
+test_data['cos'] = cos
+test_data.to_csv('data/test.csv', index = False)
 
-tfGen_test = TFGen()
+tfGen_test = TFGen(0.01)
 tfGen_test.load_data('./data/test.csv')
 # tfGen_test.diff_forward('test1', 'test1_vel')
-# tfGen_test.show_current_data()
-# tfGen_test.plot_with_cursor(time,[sin, cos], subplot_num=2)
+tfGen_test.show_current_data()
+# tfGen_test.plot_with_cursor(time,[sin, cos])
+# tfGen_test.plot_with_cursor(time,[time], logarithm=True)
 # tfGen_test.plot_with_cursor(time,[sin, cos], subplot_num=2,snap=False)
+
+# plt.semilogx(time, 20*np.log10(time))
+# plt.plot(time, time)
+# plt.show()
+
+cplx_data, gain, phase= tfGen_test.FFT_1D('sin', 'xxx')
+tfGen_test.plot_with_cursor(time,[gain, phase])
+
+
+
+# print([[1], [1]])
+
--- tf_gen.py
+++ tf_gen.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy as sp
+from scipy import fft
+import math
 
 class TFGen(object):
-    def __init__(self):
+    def __init__(self, sampling_freq):
         self.all_data = {}
+        self.sampling_freq = sampling_freq
 
     def load_data(self, file_path):
         self.all_data['raw_data'] = pd.read_table(file_path, sep=',')
@@ -12,10 +16,20 @@ class TFGen(object):
         self.all_data['processed_data'] = pd.DataFrame(index_column, columns=['index'])
         # print(self.all_data['processed_data'])
 
-    def my_FFT(self):
-        pass
+    def FFT_1D(self, input_column_name, output_column_name):
+        DF_name = self.__search_data_by_name(input_column_name)
+        if DF_name is None:
+            return DF_name
+        time_data = self.all_data[DF_name][input_column_name]
+        cplx_data = fft.fft(time_data.tolist()).tolist()
+        gain = abs(np.array(cplx_data)).tolist()
+        phase = []
+        for cplx in cplx_data:
+            phase += [math.degrees(np.arctan2(cplx.imag, cplx.real))]
 
-    def draw_bode_plot(self):
+        return cplx_data, gain, phase
+
+    def draw_bode_plot(self, input_column_name):
         pass
 
     def diff_forward(self, input_column_name, output_column_name, order = 1):
@@ -45,20 +59,37 @@ class TFGen(object):
             else:
                 print(self.all_data[name].columns.values.tolist())
 
-    def plot_with_cursor(self, x_data, y_datas, subplot_num = 1, snap = True):
+    def plot_with_cursor(self, x_data, y_datas, snap = True, logarithm = False):
+        subplot_num = np.array(y_datas).shape[0]
         fig, ax = plt.subplots(subplot_num)
-        # plt.figure()
+        ax = [ax] if subplot_num ==1 else ax
         plt.rcParams['lines.linewidth'] = '1'
         cursor_list = []
         for i in range(subplot_num):
-            ax[i].plot(x_data, y_datas[i])
+            if logarithm:
+                if min(y_datas[i]) <= 0:
+                    print('the %d# y_data contains member <=0, cannot run log()' %i)
+                    return 0
+                y_data = 20 * np.log10(y_datas[i])
+                # ax[i].semilogy(x_data, y_datas[i])
+                ax[i].semilogx(x_data, y_data)
+            else:
+                y_data = y_datas[i]
+                ax[i].plot(x_data, y_data)
             ax[i].grid()
             if snap:
-                cursor_list += [SnaptoCursor(ax[i], x_data, y_datas[i])]
+                cursor_list += [SnaptoCursor(ax[i], x_data, y_data)]
             else:
                 cursor_list += [Cursor(ax[i])]
             plt.connect('motion_notify_event', cursor_list[i].mouse_move)
         plt.show()
+
+    def __search_data_by_name(self, input_column_name):
+        for DF_name in self.all_data:
+            if input_column_name in self.all_data[DF_name].columns:
+                return DF_name
+        print('cannot find the data called: ' + input_column_name)
+        return None
 
 class Cursor(object):
     def __init__(self, ax):
